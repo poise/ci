1a23cfcad997b76666a21aef8012a1c3fb8d5d17
Noah Kantrowitz <noah@coderanger.net>
2013-12-17 00:18:33 -0800

    Update jenkins dep.

--- metadata.rb
+++ metadata.rb
@@ -25,7 +25,7 @@ license 'Apache 2.0'
 description 'Installs and configures CI server & slaves'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 
-depends 'jenkins', '~> 99.1.0'
+depends 'jenkins', '~> 99.1.2'
 depends 'git'
 depends 'partial_search'
 depends 'poise', '~> 1.0.0'
